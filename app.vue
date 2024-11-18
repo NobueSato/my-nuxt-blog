a846a7e550869bf61cd74ded3965ac88a45d6089
NobueSato <Deadmou5e23>
2024-11-18 15:06:12 -0500

    feat: modify app.vue to use index.vue

---
+++ app.vue
@@ -0,0 +1,7 @@
+<!-- app.vue -->
+<template>
+      <!-- Main content of the page (everything rendered by the route) -->
+  <div>
+    <NuxtLayout />  <!-- Nuxt will automatically inject the default layout -->
+  </div>
+</template>
