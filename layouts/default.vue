e96b7b3e33ad434d09063b31787e99e072c834b9
NobueSato <Deadmou5e23>
2024-11-18 15:06:30 -0500

    feat: create default.vue to use default layout

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<!-- layouts/default.vue -->
+<template>
+    <div>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <NuxtPage /> <!-- This is where the page content will be injected -->
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  </template>
+  
+  <script setup>
+  import Header from '~/components/Header.vue';
+  import Footer from '~/components/Footer.vue';
+  </script>
+  
